a4160190781eaa4fe0be6f1314fe383b1ad3092d
w552295 <tamer.ghattas@cs.huji.ac.il>
2021-05-08 12:59:25 +0300

    left first term + right equation terms

--- main.py
+++ main.py
@@ -28,10 +28,12 @@ def get_omega_boundary(img):
     """
     convolves img in order to calculate the discrete omega boundary as defined in the paper
     :param img: equals the mask float np 2d array
-    :return: 2d np array equals |N_p| in the pixel (2d-coords) p
+    :return:
     """
     s_without_omega = 1.0-img  # assuming img is a binary mask
-    return np.float_(4 > convolve(s_without_omega, four_neighbors_kernel, mode='nearest') > 0)
+    neighbours_count = convolve(s_without_omega, four_neighbors_kernel, mode='nearest')
+    neighbours_count = (4 > neighbours_count) * neighbours_count
+    return np.float_(neighbours_count > 0)
 
 
 def get_basic_vector_field(img, mask):
@@ -65,17 +67,26 @@ def seamless_cloning(source, target, mask, offset=None, gradient_field_source_on
 
     eq_right = sum_f_star + vector_field_sum
 
-    Np = Np * mask
-    flat_Np = Np.flatten()
     flat_mask = mask.flatten()
     flat_mask_ind = np.where(flat_mask)
-    inds = (np.arange(len(flat_mask_ind[0].shape[-1])),) + flat_mask_ind
-    sm = coo_matrix((flat_Np[np.int_(flat_mask)], inds))
+    inds = (np.arange(flat_mask_ind[0].shape[-1]),) + flat_mask_ind
 
+    Np = Np * mask
+    flat_Np = Np.flatten()
+    eq_left_sys = coo_matrix((flat_Np[flat_mask_ind], inds))
+
+    flat_eq_right = eq_right.flatten()
+    eq_right_sys = coo_matrix((flat_eq_right[flat_mask_ind], inds))
+
+    print(eq_left_sys.shape)
+    print(eq_right_sys.shape)
 
 if __name__ == '__main__':
-    img = read_image('./external/main-1.jpg', 1)
-    plt.imshow(img, cmap=plt.cm.gray)
-    plt.show()
+    target = read_image('./external/main-1.jpg', 1)
+    source = read_image('./external/blend-1.jpg', 1)
+    mask = read_image('./external/mask-1.jpg', 1)
+    seamless_cloning(source, target, mask)
+    # plt.imshow(img, cmap=plt.cm.gray)
+    # plt.show()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
