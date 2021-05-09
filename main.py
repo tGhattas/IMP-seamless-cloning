2de6c69813b1bde6945f4a4f7bf2980ff48718ef
w552295 <tamer.ghattas@cs.huji.ac.il>
2021-05-09 20:35:58 +0300

    boundary need fix

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 
-from utils import read_image
+from utils import read_image, blending_example1
 from scipy.ndimage.filters import convolve
-from scipy.sparse import coo_matrix
+from scipy.sparse import coo_matrix, dok_matrix
+
 from scipy.sparse.linalg import spsolve
 from scipy import ndimage
 import matplotlib.pyplot as plt
@@ -49,6 +50,31 @@ def get_basic_vector_field(img, mask):
     return tmp * mask
 
 
+def _to_flat_coo(r, c, cols_num):
+    return r * cols_num + c
+
+
+def mark_neighbour_variables(pixel_coo, pixel_var_ind, reference, sparse_matrix):
+    """
+
+    :param pixel_coo:
+    :param pixel_var_ind:
+    :param reference:
+    :param sparse_matrix:
+    :return:
+    """
+    r, c = pixel_coo
+    if r-1 > 0 and reference[r-1, c]:
+        sparse_matrix[pixel_var_ind, _to_flat_coo(r-1, c, reference.shape[1])] = -1.0
+    if r+1 < reference.shape[0] and reference[r+1, c]:
+        sparse_matrix[pixel_var_ind, _to_flat_coo(r+1, c, reference.shape[1])] = -1.0
+    if c-1 > 0 and reference[r, c-1]:
+        print(pixel_var_ind, _to_flat_coo(r, c-1, reference.shape[1]))
+        sparse_matrix[pixel_var_ind, _to_flat_coo(r, c-1, reference.shape[1])] = -1.0
+    if c+1 < reference.shape[1] and reference[r, c+1]:
+        sparse_matrix[pixel_var_ind, _to_flat_coo(r, c+1, reference.shape[1])] = -1.0
+
+
 def seamless_cloning(source, target, mask, offset=None, gradient_field_source_only=True):
     """
     :param source:
@@ -58,43 +84,61 @@ def seamless_cloning(source, target, mask, offset=None, gradient_field_source_on
     :param gradient_field_source_only:
     :return:
     """
+    mask = mask > 0
     Np = get_4_neigbours_amount(target) * mask  # for the calc of left first term in equation (7)
-
     omega_boundary = get_omega_boundary(mask)
-
     sum_f_star = convolve(omega_boundary * target, four_neighbors_kernel, mode='constant', cval=0.0) * mask  # for the
     # calc of right first term in equation (7)
-
     vector_field_sum = get_basic_vector_field(source, mask)  # for the calc of right second term in equation (7)
-
     eq_right = sum_f_star + vector_field_sum
 
     flat_mask = mask.flatten()
-    flat_mask_ind = np.where(flat_mask)
-    omega_vars_indices = (np.arange(flat_mask_ind[0].shape[-1]),) + flat_mask_ind
-    print(flat_mask_ind[0].shape[-1])
-    print(flat_mask_ind)
-    Np = Np * mask
-    eq_left_1 = Np
-    eq_left_2 = ndimage.binary_dilation(Np, structure=four_neighbors_kernel).astype(np.float32)
-    eq_left = eq_left_1 - eq_left_2
-    flat_eq_left = eq_left.flatten()
-    eq_left_sys = coo_matrix((flat_eq_left[flat_mask_ind], omega_vars_indices))
+    flat_mask_ind = np.where(flat_mask > 0)  # returns all pixels indices
+
+    flat_eq_left_1 = Np.flatten()
+    diag_indices = (np.arange(flat_mask_ind[0].shape[-1]), np.arange(flat_mask_ind[0].shape[-1]))
+    eq_left_1_sys = coo_matrix((flat_eq_left_1[flat_mask_ind], diag_indices))
+    eq_left_2_reference = ndimage.binary_dilation(Np, structure=four_neighbors_kernel).astype(np.float64) * mask
+    eq_left_sys = dok_matrix(eq_left_1_sys)
+
+    # go over pixels and check
+    for pixel_var_ind in diag_indices[-1]:
+        pixel_coo = pixel_var_ind // Np.shape[1], pixel_var_ind % Np.shape[1]
+        mark_neighbour_variables(pixel_coo, pixel_var_ind, eq_left_2_reference, eq_left_sys)
+
+    eq_left_sys = eq_left_sys.tocsr()
 
     flat_eq_right = eq_right.flatten()
-    eq_right_sys = coo_matrix((flat_eq_right[flat_mask_ind], omega_vars_indices))
-
     print(eq_left_sys.shape)
-    print(eq_right_sys.shape)
-    f = spsolve(eq_left_sys, eq_right_sys)
+    print(flat_eq_right[flat_mask_ind].shape)
+    print('max', np.max(flat_eq_right))
+    f = spsolve(eq_left_sys, flat_eq_right[flat_mask_ind]).astype(np.float64)
     print(f)
+
+    # reconstruct image
+    blend = target.flatten()
+    g = source.flatten()
+    blend[flat_mask_ind] = f + g[flat_mask_ind]
+    blend = blend.reshape(target.shape)
+    blend[omega_boundary > 0] -= source[omega_boundary>0]
+
+    plt.imshow(omega_boundary, cmap=plt.cm.gray)
+    plt.show()
+
+    aux = blend.copy()
+    aux[omega_boundary>0]=1
+    return blend, aux
+
 
 if __name__ == '__main__':
     target = read_image('./external/main-1.jpg', 1)
     source = read_image('./external/blend-1.jpg', 1)
     mask = read_image('./external/mask-1.jpg', 1)
-    seamless_cloning(source, target, mask)
-    # plt.imshow(img, cmap=plt.cm.gray)
-    # plt.show()
+    cloned, aux = seamless_cloning(source, target, mask)
+    plt.imshow(cloned, cmap=plt.cm.gray)
+    plt.show()
+    plt.imshow(aux, cmap=plt.cm.gray)
+    plt.show()
+    blending_example1()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
