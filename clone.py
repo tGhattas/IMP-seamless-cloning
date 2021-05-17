6f6448379158a9428b9b9272022e6f64a487d1ab
tGhattas <tammer.ghattas@gmail.com>
2021-05-17 19:47:02 +0300

    added GUI, result is wrong. needs fix

--- clone.py
+++ clone.py
@@ -2,7 +2,6 @@ from utils import read_image, pyramid_blending_example1, plot, plt
 from scipy.ndimage.filters import convolve
 from scipy.sparse import lil_matrix, block_diag
 from scipy.sparse.linalg import spsolve
-from numpy.linalg import solve
 from scipy.ndimage.morphology import distance_transform_edt
 from scipy import ndimage, fft
 from tqdm import tqdm
